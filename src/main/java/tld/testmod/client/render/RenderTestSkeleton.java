36302d4c53494282ab960a75283c15797b4afea3
Aeronica <pmboese@gmail.com>
2017-04-28 12:37:29 -0500

    Update the golden skeleton to extend EntitySkeleton instead of extending a copy of the AbstractSkeleton vanilla class. The aim is to be more compatible with 1.10,2.

--- src/main/java/tld/testmod/client/render/RenderTestSkeleton.java
+++ src/main/java/tld/testmod/client/render/RenderTestSkeleton.java
@@ -1,5 +1,6 @@
 package tld.testmod.client.render;
 
+import net.minecraft.client.model.ModelSkeleton;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderBiped;
 import net.minecraft.client.renderer.entity.RenderManager;
@@ -9,24 +10,23 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import tld.testmod.Main;
-import tld.testmod.client.model.ModelTestSkeleton;
-import tld.testmod.common.entity.living.AbstractTestSkeleton;
+import tld.testmod.common.entity.living.EntityTestSkeleton;
 
 @SideOnly(Side.CLIENT)
-public class RenderTestSkeleton extends RenderBiped<AbstractTestSkeleton>
+public class RenderTestSkeleton extends RenderBiped<EntityTestSkeleton>
 {
     private static final ResourceLocation SKELETON_TEXTURES = new ResourceLocation(Main.MODID, "textures/entity/skeleton/test_skeleton.png");
 
     public RenderTestSkeleton(RenderManager renderManagerIn)
     {
-        super(renderManagerIn, new ModelTestSkeleton(), 0.5F);
+        super(renderManagerIn, new ModelSkeleton(), 0.5F);
         this.addLayer(new LayerHeldItem(this));
         this.addLayer(new LayerBipedArmor(this)
         {
             protected void initArmor()
             {
-                this.modelLeggings = new ModelTestSkeleton(0.5F, true);
-                this.modelArmor = new ModelTestSkeleton(1.0F, true);
+                this.modelLeggings = new ModelSkeleton(0.5F, true);
+                this.modelArmor = new ModelSkeleton(1.0F, true);
             }
         });
     }
@@ -39,7 +39,7 @@ public class RenderTestSkeleton extends RenderBiped<AbstractTestSkeleton>
     /**
      * Returns the location of an entity's texture. Doesn't seem to be called unless you call Render.bindEntityTexture.
      */
-    protected ResourceLocation getEntityTexture(AbstractTestSkeleton entity)
+    protected ResourceLocation getEntityTexture(EntityTestSkeleton entity)
     {
         return SKELETON_TEXTURES;
     }
