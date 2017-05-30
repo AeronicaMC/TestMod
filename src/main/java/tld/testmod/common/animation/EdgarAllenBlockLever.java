1581ec29fe6d3a867bbc01c4561f382a5d222052
Aeronica <pmboese@gmail.com>
2017-05-30 09:55:41 -0500

    Rename EdgarAllen/ForgeAnimationSystemTests appropriately.

---
+++ src/main/java/tld/testmod/common/animation/EdgarAllenBlockLever.java
@@ -0,0 +1,119 @@
+/**
+ * Copyright {2016} Paul Boese aka Aeronica
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package tld.testmod.common.animation;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import net.minecraftforge.common.property.ExtendedBlockState;
+import net.minecraftforge.common.property.IUnlistedProperty;
+import net.minecraftforge.common.property.Properties;
+import tld.testmod.Main;
+
+/*
+ * https://github.com/EdgarAllen/ForgeAnimationSystemTests
+ */
+public class EdgarAllenBlockLever extends Block
+{
+
+    public static final PropertyDirection FACING = PropertyDirection.create("facing", EnumFacing.Plane.HORIZONTAL);
+    
+    public EdgarAllenBlockLever()
+    {
+        this(Material.WOOD);
+        setCreativeTab(Main.MODTAB);
+    }
+    
+    public EdgarAllenBlockLever(Material materialIn)
+    {
+        super(materialIn);
+    }
+
+    @Override
+    public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ)
+    {
+        if(world.isRemote)
+        {
+            TileEntity te = world.getTileEntity(pos);
+            if(te instanceof EdgarAllenTileEntity)
+            {
+                ((EdgarAllenTileEntity)te).click();
+            }
+        }
+        return true;
+    }
+    
+    @Override
+    public ExtendedBlockState createBlockState()
+    {
+        return new ExtendedBlockState(this, new IProperty[]{ FACING, Properties.StaticProperty }, new IUnlistedProperty[]{ Properties.AnimationProperty });
+    }
+    
+    @Override
+    public boolean isOpaqueCube(IBlockState state) { return false; }
+
+    @Override
+    public boolean isFullCube(IBlockState state) { return false; }
+
+    @Override
+    public IBlockState getStateForPlacement(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer)
+    {
+        return this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
+    }
+    
+    @Override
+    public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack)
+    {
+        this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
+    }
+
+    @Override
+    public IBlockState getStateFromMeta(int meta) {
+        return getDefaultState().withProperty(FACING, EnumFacing.getFront(meta));
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state) {
+        return ((EnumFacing)state.getValue(FACING)).getIndex();
+    }
+
+    @Override
+    public boolean hasTileEntity(IBlockState state) {
+        return true;
+    }
+
+    @Override
+    public TileEntity createTileEntity(World world, IBlockState state) {
+        return new EdgarAllenTileEntity();
+    }
+
+    @Override
+    public IBlockState getActualState(IBlockState state, IBlockAccess world, BlockPos pos) {
+        return state.withProperty(Properties.StaticProperty, true);
+    }
+
+}
---
+++ src/main/java/tld/testmod/common/animation/EdgarAllenItemBlock.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright {2016} Paul Boese aka Aeronica
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package tld.testmod.common.animation;
+
+import javax.annotation.Nullable;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import tld.testmod.Main;
+
+/*
+ * https://github.com/EdgarAllen/ForgeAnimationSystemTests
+ */
+public class EdgarAllenItemBlock extends ItemBlock
+{
+
+    public EdgarAllenItemBlock(Block block)
+    {
+        super(block);
+        setHasSubtypes(false);
+        setCreativeTab(Main.MODTAB);
+    }
+
+    @Override
+    public ICapabilityProvider initCapabilities(ItemStack stack, @Nullable NBTTagCompound nbt)
+    {
+        return new EdgarAllenItemAnimationHolder();
+    }
+}
