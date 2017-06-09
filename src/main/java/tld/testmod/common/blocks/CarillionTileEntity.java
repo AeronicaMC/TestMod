c44b38cc086ff2c659f893ca3d8569c029b0db31
Aeronica <pmboese@gmail.com>
2017-06-08 20:44:00 -0500

    Experiment with accessing tile entities near a controlling block. In this case setting the pitch of the oneshot bell blocks and trigging them to play.

--- src/main/java/tld/testmod/common/blocks/CarillionTileEntity.java
+++ src/main/java/tld/testmod/common/blocks/CarillionTileEntity.java
@@ -1,8 +1,13 @@
 package tld.testmod.common.blocks;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
 import tld.testmod.ModLogger;
+import tld.testmod.common.animation.OneShotTileEntity;
+import tld.testmod.init.ModBlocks;
 
 public class CarillionTileEntity extends TileEntity
 {
@@ -15,7 +20,37 @@ public class CarillionTileEntity extends TileEntity
 
     public void triggerCarillon()
     {
-        ModLogger.info("Carillon triggered! %s", world.isRemote ? "client" : "server");
+        int offset = 3;
+        IBlockState state = world.getBlockState(pos);
+        EnumFacing facing = (EnumFacing) state.getValue(BlockCarillon.FACING);
+        BlockCarillon blockCarillon = (BlockCarillon) state.getBlock();
+        BlockPos.MutableBlockPos mpos = new BlockPos.MutableBlockPos(pos);
+        BlockPos.MutableBlockPos opos = new BlockPos.MutableBlockPos(pos);
+        IBlockState mState;
+        OneShotTileEntity osTE;
+
+        byte pitch = 0;
+        for (int x =0; x<3; x++)
+            for (int z = 0; z<3; z++)
+                for (int y=0; y<3; y++)
+                {
+                    opos.setPos(pos.offset(facing.getOpposite(), offset).add(x,y,z));
+                    mState = world.getBlockState(opos);
+                    if (mState.getBlock() == ModBlocks.ONE_SHOT)
+                    {
+                        if (ModBlocks.ONE_SHOT.hasTileEntity(mState))
+                        {
+                            if((osTE = ((OneShotTileEntity) world.getTileEntity(opos))) != null)
+                            {
+                                osTE.setPitch(pitch++);
+                                ModLogger.info("OneShot Bell Found! %s, %s, %d", mState, opos, osTE.getPitch());
+                                osTE.triggerOneShot();
+                                if (pitch > 24) break;
+                            }
+                        }
+                    }
+                }
+        
     }
 
     /**
