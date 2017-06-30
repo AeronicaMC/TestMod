a63b41d126f2d444138f07eb7e969da4cfc220c4
Aeronica <pmboese@gmail.com>
2017-06-30 10:26:25 -0500

    Clean up entity registrations and remove spawn eggs where not needed or appropriate. Update lang file.

--- src/main/java/tld/testmod/Main.java
+++ src/main/java/tld/testmod/Main.java
@@ -28,6 +28,7 @@ public class Main
     public static final String MODID = "testmod";
     public static final String MODNAME = "Test Mod";
     public static final String VERSION = "{@VERSION}";
+    static int modEntityId = 0;
     
     @Mod.Instance(MODID)
     public static Main instance;
@@ -52,14 +53,14 @@ public class Main
     public void init(FMLInitializationEvent event)
     {
         proxy.init(event);
-        EntityRegistry.registerModEntity(new ResourceLocation(MODID,  "test_skeleton"), EntityGoldenSkeleton.class, "test_skeleton", 0, this, 64, 1, true, 0x000000, 0xE6BA50);
-        EntityRegistry.registerModEntity(new ResourceLocation(MODID,  "mob_timpani"), EntityTimpani.class, "mob_timpani", 1, this, 64, 1, true, 0x000000, 0xFF5121);
+        EntityRegistry.registerModEntity(new ResourceLocation(MODID,  "test_skeleton"), EntityGoldenSkeleton.class, "test_skeleton", getNextModEntityId(), this, 64, 1, true, 0x000000, 0xE6BA50);
+        EntityRegistry.registerModEntity(new ResourceLocation(MODID,  "mob_timpani"), EntityTimpani.class, "mob_timpani", getNextModEntityId(), this, 64, 1, true, 0x000000, 0xFF5121);
         
-        EntityRegistry.registerModEntity(new ResourceLocation(MODID, "entity_forge_anim_test"), ForgeAnimEntity.class, "entity_forge_anim_test", 2, this, 64, 20, true, 0xFFAAAA00, 0xFFDDDD00);
-        EntityRegistry.registerModEntity(new ResourceLocation(MODID, "entity_edgar_allen_block_lever"), EdgarAllenAnimEntity.class, "entity_edgar_allen_block_lever", 3, this, 64, 20, true, 0xFFAAAA00, 0xFFDDDD00);
-        EntityRegistry.registerModEntity(new ResourceLocation(MODID, "entity_one_shot"), OneShotEntity.class, "entity_one_shot", 4, this, 64, 20, true, 0xFFAAAA00, 0xFFDDDD00);
+        EntityRegistry.registerModEntity(new ResourceLocation(MODID, "entity_forge_anim_test"), ForgeAnimEntity.class, "entity_forge_anim_test", getNextModEntityId(), this, 64, 20, true);
+        EntityRegistry.registerModEntity(new ResourceLocation(MODID, "entity_edgar_allen_block_lever"), EdgarAllenAnimEntity.class, "entity_edgar_allen_block_lever", getNextModEntityId(), this, 64, 20, true);
+        EntityRegistry.registerModEntity(new ResourceLocation(MODID, "entity_one_shot"), OneShotEntity.class, "entity_one_shot", getNextModEntityId(), this, 64, 20, true);
 
-        EntityRegistry.registerModEntity(new ResourceLocation(MODID, "entity_pull"), EntityPull.class, "entity_pull", 5, this, 64, 20, true, 0xFFAAAA00, 0xFFDDDD00);
+        EntityRegistry.registerModEntity(new ResourceLocation(MODID, "entity_pull"), EntityPull.class, "entity_pull", getNextModEntityId(), this, 64, 20, true);
     }
 
     @EventHandler
@@ -68,6 +69,11 @@ public class Main
         proxy.postInit(event);
     }
 
+    private int getNextModEntityId()
+    {
+        return modEntityId++;
+    }
+    
     public static String prependModID(String name)
     {
         return MODID + ":" + name;
