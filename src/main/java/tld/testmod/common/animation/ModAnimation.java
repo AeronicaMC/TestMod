3458a6b6ca730aeb3c84a8a3919ed2d81f43def4
Aeronica <pmboese@gmail.com>
2017-10-05 17:00:02 -0500

    Use the world hashcode instead of the world instance.

--- src/main/java/tld/testmod/common/animation/ModAnimation.java
+++ src/main/java/tld/testmod/common/animation/ModAnimation.java
@@ -2,14 +2,13 @@ package tld.testmod.common.animation;
 
 import net.minecraft.world.World;
 import net.minecraftforge.client.model.animation.Animation;
-import tld.testmod.ModLogger;
 
 public enum ModAnimation
 {
 
     INSTANCE;
     static long timeOffset;
-    static Object lastWorld;
+    static int lastWorldHashCode;
     
     /**
      * Get the global world time for the current tick, in seconds.
@@ -24,13 +23,14 @@ public enum ModAnimation
      */
     public static float getWorldTime(World world, float tickProgress)
     {
-        if (!world.equals(lastWorld))
+        int worldHashCode = world.hashCode();
+        if (worldHashCode != lastWorldHashCode)
         {
             timeOffset = world.getTotalWorldTime();
-            lastWorld = world;
+            lastWorldHashCode = worldHashCode;
         }
-        long diff = world.getTotalWorldTime() - timeOffset;
-        //ModLogger.info("Animation#getWorldTime: World: %s, time: %d, offset %d, diff: %d", lastWorld, world.getTotalWorldTime(), timeOffset, diff);
+        //long diff = world.getTotalWorldTime() - timeOffset;
+        //ModLogger.info("Animation#getWorldTime: World: %s, time: %d, offset %d, diff: %d", lastWorldHashCode, world.getTotalWorldTime(), timeOffset, diff);
         return (world.getTotalWorldTime() - timeOffset  + tickProgress) / 20;
     }
 
