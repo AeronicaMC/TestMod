641e0cfbd0de99b933be31030edb188d1159c0fa
Aeronica <pmboese@gmail.com>
2020-06-25 16:50:23 -0500

    Tooltip event test

--- src/main/java/tld/testmod/client/ClientEventHandler.java
+++ src/main/java/tld/testmod/client/ClientEventHandler.java
@@ -6,17 +6,21 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.RenderGlobal;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.common.ForgeHooks;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
+import tld.testmod.init.ModItems;
 import tld.testmod.library.LocationArea;
 import tld.testmod.common.items.IPlaceableBounding;
 
@@ -103,6 +107,16 @@ public class ClientEventHandler
                 LocationArea boundingBox = blockPlacer.getBoundingBox(player, world, pos);
                 renderBox(boundingBox.getStartingPoint(), boundingBox.getStartPointPlusSize(), event.getPartialTicks());            
             }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onEvent(ItemTooltipEvent event)
+    {
+        if (event.getItemStack().getItem().equals(ModItems.ITEM_MULTI_TEX))
+
+        {
+            event.getToolTip().add(TextFormatting.GREEN + I18n.format("item.testmod:multitex.help"));
         }
     }
 }
