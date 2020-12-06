ffeb309952c49ecd002664f534c89bd35f9efe98
Aeronica <pmboese@gmail.com>
2020-12-06 13:07:15 -0600

    Enforce UTF-8 for GUID SHA-256 phrase string.

--- src/main/java/tld/testmod/common/utils/GUID.java
+++ src/main/java/tld/testmod/common/utils/GUID.java
@@ -3,6 +3,7 @@ package tld.testmod.common.utils;
 
 import tld.testmod.ModLogger;
 
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Objects;
@@ -90,7 +91,7 @@ public class GUID implements java.io.Serializable, Comparable<GUID>
         try
         {
             md = MessageDigest.getInstance("SHA-256");
-            md.update(phrase.getBytes());
+            md.update(phrase.getBytes(StandardCharsets.UTF_8));
             return new GUID(md.digest());
         }
         catch (NoSuchAlgorithmException e)
